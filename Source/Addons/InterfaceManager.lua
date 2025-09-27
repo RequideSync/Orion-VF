90639b9851c1f5bb3f4fcc45f0888c4271aa6317
RequideSync <ronixroblox6666@gmail.com>
2025-09-27 13:49:11 +0900

    Update InterfaceManager.lua

--- Source/Addons/InterfaceManager.lua
+++ Source/Addons/InterfaceManager.lua
@@ -1,7 +1,17 @@
 local InterfaceManager = {}
+
+_G.setclipboard = write_clipboard or writeclipboard or setclipboard or set_clipboard or nil
+_G.request = http_request or request or httprequest or nil
+_G.readfile = readfile or read_file or nil
+_G.writefile = writefile or write_file or nil
+_G.isfile = isfile or is_file or nil
+
+local ConfigFile = "ThemeConfig.json"
+
 local Hensuu = {
 	SelectedTheme = "Default",
 }
+
 local Theme = {
 	"Default",
 	"Bliz_T",
@@ -13,8 +23,31 @@ local Theme = {
 	"Retro",
 }
 
+local function SaveConfig()
+	if _G.writefile then
+		local data = game:GetService("HttpService"):JSONEncode(Hensuu)
+		_G.writefile(ConfigFile, data)
+	end
+end
+
+local function LoadConfig()
+	if _G.isfile and _G.readfile and _G.isfile(ConfigFile) then
+		local success, data = pcall(function()
+			return game:GetService("HttpService"):JSONDecode(_G.readfile(ConfigFile))
+		end)
+		if success and data.SelectedTheme then
+			Hensuu.SelectedTheme = data.SelectedTheme
+		end
+	end
+end
+
+LoadConfig()
+
 function InterfaceManager:SetLibrary(Library)
 	self.Library = Library
+	if self.Library and self.Library.SetTheme then
+		self.Library.SetTheme(Hensuu.SelectedTheme)
+	end
 end
 
 function InterfaceManager:BuildInterfaceSection(Tabs)
@@ -28,13 +61,15 @@ function InterfaceManager:BuildInterfaceSection(Tabs)
 		Options = Theme,
 		Callback = function(Value)
 			Hensuu.SelectedTheme = Value
+			SaveConfig()
 		end    
 	})
-	
+
 	Tabs:AddButton({
 		Name = "Apply Theme",
 		Callback = function()
 			Library.SetTheme(Hensuu.SelectedTheme)
+			SaveConfig()
 		end    
 	})
 end
